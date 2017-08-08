df6fad77c82d7697f5760979cf631ada5144c890
pathakganga11 <pathakganga11@gmail.com>
2017-08-08 17:09:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
+//change content 
 var element = document.getElementById("main-text");
 element.innerHTML = "new value";
+//move the image when user click
+var element = document.getElementByID("img");
+img.onclick = function() {
+    img.style.marginleft = '100px';
+}
