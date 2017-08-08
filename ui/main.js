bcce7398cf306547fe1f253dd733f9078d3f3cea
pathakganga11 <pathakganga11@gmail.com>
2017-08-08 17:35:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,5 +10,5 @@ function moveRight() {
     img.style.marginleft = marginleft + 'px';
 }
 img.onclick = function (){
-    var interval = setInterval(moveRight , 100);
+    var interval = setInterval(moveRight , 50);
 };
