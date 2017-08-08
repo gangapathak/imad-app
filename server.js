f76e0c198be19e4b62d4ea143e264dde06bdd9b5
pathakganga11 <pathakganga11@gmail.com>
2017-08-08 15:03:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,12 +4,57 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-
+var articeOne = {
+    title : 'Article one by Ganga Pathak',
+    heading: 'Article One',
+    date: '08-08-2017',
+    content:`
+    <p>
+        IMAD (Introduction to Modern Application Development) is a MOOC on how to build web/mobile applications offered by Hasura, under it's banner Hasura Education. IMAD is offered in partnership with IIT Madras
+        </p>
+        <p>
+        If you just want to experience the joy of building an app, or have dreams of finding a job at one of the hottest tech companies or actually building one or just understand the app world that you cannot escape, you have come to the right place!
+        </p>
+        <p>
+        We believe building technology can be both fun and empowering. We also think one can never be too young or too old to learn new things, and that applies to technology as well!
+        </p>`
+};
+function createTemplate(data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = date.content;
+htmlTemplate = `
+<head>
+     <title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="/ui/style.css" rel="stylesheet" />
+         <title>${title}</title>
+             </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href = "/">home</a>
+        </div>
+        <hr/>
+        <h3>
+           ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+        ${content}
+        </div>
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(artileOne));
 });
 app.get('/article-two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
@@ -34,3 +79,4 @@ var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+2
