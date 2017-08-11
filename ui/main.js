7499dce0df59d765fb31a85d7c21eaad182ff2cd
pathakganga11 <pathakganga11@gmail.com>
2017-08-11 15:10:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,16 @@
-console.log('Loaded!');
+//console.log('Loaded!');
 //change content 
-var element = document.getElementById("main-text");
+//var element = document.getElementById("main-text");
 element.innerHTML = "Hi I am Madi";
 
 //move the image when user click
-var img = document.getElementById('abc');
- img.onclick = function() {
-    image.style.marginLeft = '100px';
+//var img = document.getElementById('abc');
+// img.onclick = function() {
+  //  image.style.marginLeft = '100px';
+//};
+var button = document.getElementById('counter');
+button.onclick = function(){
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 };
